90120048c5d67f24329389864cd24316faef2731
micheleldsk <micheleldsk@gmail.com>
2023-07-10 20:33:02 -0300

    interface com generics

--- Alura-Ts/app/src/models/negociacoes.ts
+++ Alura-Ts/app/src/models/negociacoes.ts
@@ -1,7 +1,8 @@
+import { Comparavel } from "../interfaces/comparavel.js";
 import { Imprimivel } from "../utils/imprimivel.js";
 import { Negociacao } from "./negociacao.js";
 
-export class Negociacoes implements Imprimivel {
+export class Negociacoes implements Imprimivel, Comparavel<Negociacoes> {
   //    private negociacoes: Array<Negociacao>
   private negociacoes: Negociacao[] = [];
 
@@ -16,5 +17,11 @@ export class Negociacoes implements Imprimivel {
 
   public paraTexto(): string {
     return JSON.stringify(this.negociacoes, null, 2);
+  }
+
+  public ehIgual(negociacoes: Negociacoes): boolean {
+    return (
+      JSON.stringify(this.negociacoes) === JSON.stringify(negociacoes.lista())
+    );
   }
 }
