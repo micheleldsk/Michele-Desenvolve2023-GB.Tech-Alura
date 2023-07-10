b4f9ab6f8f73024937273b23225e533d29c2c6e7
micheleldsk <micheleldsk@gmail.com>
2023-07-10 18:27:03 -0300

    upgarde no domInjector para usar o cache;

--- Alura-Ts/app/src/decorators/dom-injector.ts
+++ Alura-Ts/app/src/decorators/dom-injector.ts
@@ -1,17 +1,27 @@
-export function domInjector(setletor: string) {
+export function domInjector(seletor: string) {
   return function (target: any, propertyKey: string) {
     console.log(
       `Modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}.`
     );
 
+    let elemento: HTMLElement;
     const getter = function () {
-      const elemento = document.querySelector(setletor);
-      console.log(
-        `Buscando elemento do DOM com o seletor ${setletor} para injetar em ${propertyKey}.`
-      );
-
+      if (!elemento) {
+        elemento = <HTMLElement>document.querySelector(seletor);
+        console.log(
+          `Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}.`
+        );
+      }
       return elemento;
     };
+
+    //   const elemento = document.querySelector(setletor);
+    //   console.log(
+    //     `Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}.`
+    //   );
+
+    //   return elemento;
+    // };
 
     Object.defineProperty(target, propertyKey, { get: getter });
   };
